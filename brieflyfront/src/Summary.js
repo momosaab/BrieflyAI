2ae8c63c7a011b7592e77e3b7077135760cb574b
Mohammed Saab <mohammed.saab40@myhunter.cuny.edu>
2024-05-31 19:10:56 -0400

    Loading Screen Implemented, summarazation working and UI updates

---
+++ brieflyfront/src/Summary.js
@@ -0,0 +1,14 @@
+// Summary.js
+
+import React from 'react';
+
+const Summary = ({ summary }) => {
+  return (
+    <div className="summary">
+      <h2>Summary</h2>
+      <p>{summary}</p>
+    </div>
+  );
+}
+
+export default Summary;
